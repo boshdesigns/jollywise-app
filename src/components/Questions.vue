3a61e3e609fc5451b8225d333cbf4dfc07f158b5
Ash Holley <ashley.holley@fridaymediagroup.com>
2018-03-05 17:00:58 +0000

    Submit error sorted: now adding loader and sumbit message

--- src/components/Questions.vue
+++ src/components/Questions.vue
@@ -4,26 +4,27 @@
       <div class="col-sm-12">
         <h1>Jollywise App</h1>
 
-        <!-- Form Step 1 -->
-        <form id="step1" class="form form--questions" :class="[(formState === 'step1') ? 'form--show' : 'form--hidden']">
+        <section :class="[(formState === 'step1') ? 'form--show' : 'form--hidden']">
+          <!-- Form Step 1 -->
+          <form id="step1" class="form form--questions">
 
-          <div class="form-group" v-for="(value, key, index) in form.step1">
-            <label :for="'question' + (index + 1)">Question {{index + 1}} : {{value.question}}</label>
-            <section class="container">
-              <input :id="'question' + (index + 1)" v-model="value.answer" type="text" class="form-control" placeholder="Enter your answer...">
-            </section>
-          </div>
+            <div class="form-group" v-for="(value, key, index) in form.step1">
+              <label :for="'question' + (index + 1)">Question {{index + 1}} : {{value.question}}</label>
+              <section class="container">
+                <input :id="'question' + (index + 1)" v-model="value.answer" type="text" class="form-control" placeholder="Enter your answer...">
+              </section>
+            </div>
 
-          <button v-on:click.self.prevent="changeFormState()" type="submit" class="btn btn-primary">Next</button>
+            <button v-on:click.self.prevent="changeFormState()" type="submit" class="btn btn-primary">Next</button>
 
-        </form>
+          </form>
+        </section>
 
-
-        <!-- Form Step 2 -->
-        <form id="step2" class="form form--sumbit" :class="[(formState === 'step2') ? 'form--show' : 'form--hidden']">
-          <aside>
-            <ul>
-              <li v-for="(value, key, index) in form.step1">
+        <section :class="[(formState === 'step2') ? 'form--show' : 'form--hidden']">
+          <!-- Step1 Answers + Form Validate -->
+          <aside class="step1-answers">
+            <ul class="step1-answers__list">
+              <li class="step1-answers__answer" v-for="(value, key, index) in form.step1">
                 <div>{{value.question}}</div><div>{{"Answer: " + value.answer}}</div>
               </li>
             </ul>
@@ -35,26 +36,35 @@
             </li>
           </ul>
 
-          <div class="form-group" v-for="(value, key, index) in form.step2">
-            <label :for="key">Your {{ key | capitalise }}</label>
-            <section class="container" v-if="key === 'firstname'">
-              <input :id="value.value" v-model="value.value" type="text" class="form-control" placeholder="Enter your firstname...">
-            </section>
-            <section class="container" v-else-if="key === 'lastname'">
-              <input :id="value.value" v-model="value.value" type="text" class="form-control" placeholder="Enter your lastname...">
-            </section>
-            <section class="container" v-else-if="key === 'email'">
-              <input :id="value.value" v-model="value.value" type="text" class="form-control" placeholder="Enter your email address...">
-            </section>
-          </div>
+          <!-- Form Step 2 -->
+          <form id="step2" class="form form--sumbit" >
+            <div class="form-group" v-for="(value, key, index) in form.step2">
+              <label :for="key">Your {{ key | capitalise }}</label>
+              <section class="container" v-if="key === 'firstname'">
+                <input :id="value.value" v-model="value.value" type="text" class="form-control" placeholder="Enter your firstname...">
+              </section>
+              <section class="container" v-else-if="key === 'lastname'">
+                <input :id="value.value" v-model="value.value" type="text" class="form-control" placeholder="Enter your lastname...">
+              </section>
+              <section class="container" v-else-if="key === 'email'">
+                <input :id="value.value" v-model="value.value" type="text" class="form-control" placeholder="Enter your email address...">
+              </section>
+            </div>
 
-          <button v-on:click.self.prevent="changeFormState()" type="submit" class="btn btn-primary">Back</button>
-          <button v-on:click.self.prevent="validate()" type="submit" class="btn btn-primary">Submit</button>
-        </form>
+            <button v-on:click.self.prevent="changeFormState()" type="submit" class="btn btn-primary">Back</button>
+            <button v-on:click.self.prevent="validate()" type="submit" class="btn btn-primary">Submit</button>
+
+            <section v-if="sumbitLoader == true" class="loader"></section>
+          
+          </form>
+        </section>
+
       </div>
     </div>
   </section>
 </template>
+
+
 
 <script>
 // Import the baseURL from https-config
@@ -66,6 +76,8 @@ export default {
     return {
       formState: 'step1',
       validateErrors: [],
+      submitLoader: false,
+      submitMessage: '',
       form: {
         step1: {
           q1: {
@@ -140,6 +152,9 @@ export default {
 
     submitResponse: function () {
 
+      // Set a loader SVG whilst submitting
+      this.submitLoader = ture
+
       // Rip out the answers so these sit in their own object
       let answers = []
 
@@ -167,13 +182,28 @@ export default {
         "_xss_cookie": "none"
       }
 
-      // test setting up FormData
-      var formData = new FormData();
-      formData.append('file', 'file');
+      // Set up the formData to sumbit
+      var formData = new FormData(postData);
 
+      // Lets actually set the data to formData
+      // This might actually help in submitting the data ** Cheers Paul!
+      for ( var key in postData ) {
+        formData.append(key, postData[key]);
+      }
+
+      for (var value of formData.values()) {
+         console.log(value);
+      }
+
+
+      // Lastly send the data
       HTTPS.post('ashtest', formData)
         .then(response => {
           console.log(response.data);
+          if(response.data.result === "SUCCESS") {
+            this.submitLoader = false
+            this.submitMessage = true
+          }
         })
         .catch(e => {
           this.postErrors.push(e)
@@ -192,7 +222,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-aside {
+.step1-answers {
   background-color: white;
   -webkit-box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.15);
   -moz-box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.15);
@@ -301,6 +331,25 @@ input {
     bottom: 20px;
   }
   padding: 0;
+}
+
+#step2 {
+  height: 100%;
+  position: relative;
+
+  .loader {
+    background: {
+      color: hsla(0, 0%, 0%, 0.7);
+      repeat: no-repeat;
+      image: url('../assets/oval.svg');
+      position: center center;
+    }
+    height: 100%;
+    position: absolute;
+    top: 0;
+    width: 100%;
+    left: 0;
+  }
 }
 
 [v-cloak] {
